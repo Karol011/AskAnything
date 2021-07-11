eff8348566967324f6c1cb9bd9e7e350d0d7af56
Karol011 <karollacek1@gmail.com>
2021-07-11 09:45:53 +0200

    initial commit

---
+++ src/main/java/pl/sda/askanything/AskAnythingApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.askanything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskAnythingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskAnythingApplication.class, args);
+    }
+
+}
