05993691bdfaecc2b51b6f7d2bf980f53d8005b1
Karol011 <karollacek1@gmail.com>
2021-09-14 23:15:06 +0200

    cleanup

--- src/test/java/pl/sda/askanything/AskAnythingApplicationTests.java
+++ src/test/java/pl/sda/askanything/AskAnythingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AskAnythingApplicationTests {
 
-   /* @Test
+    @Test
     void contextLoads() {
-    }*/
+    }
 
 }
