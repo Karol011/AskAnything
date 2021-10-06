5d26187ef8a0cac022749683ce42e2cfd6590cf4
Karol011 <karollacek1@gmail.com>
2021-10-06 09:06:55 +0200

    added and configured swagger ui

--- src/main/java/pl/sda/askanything/AskAnythingApplication.java
+++ src/main/java/pl/sda/askanything/AskAnythingApplication.java
@@ -2,11 +2,28 @@ package pl.sda.askanything;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AskAnythingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AskAnythingApplication.class, args);
+    }
+
+    @Bean
+    public Docket get() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                //create documentation only for classes with @RestController annotation
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .build();
     }
 }
