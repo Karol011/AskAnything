2d13c3a85bb1267e87d5853e2d910f71c641d979
Karol011 <karollacek1@gmail.com>
2021-10-14 20:38:15 +0200

    moved docket(swagger) configuration to separate class

--- src/main/java/pl/sda/askanything/AskAnythingApplication.java
+++ src/main/java/pl/sda/askanything/AskAnythingApplication.java
@@ -2,12 +2,6 @@ package pl.sda.askanything;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -18,12 +12,5 @@ public class AskAnythingApplication {
         SpringApplication.run(AskAnythingApplication.class, args);
     }
 
-    @Bean
-    public Docket get() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                //create documentation only for classes with @RestController annotation
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .build();
-    }
+
 }
