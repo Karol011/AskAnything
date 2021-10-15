e0298e67c1b2fe59a7371d826c026afb44744862
Karol011 <karollacek1@gmail.com>
2021-10-15 19:11:26 +0200

    removed deprecated @  @Bean

--- src/main/java/pl/sda/askanything/AskAnythingApplication.java
+++ src/main/java/pl/sda/askanything/AskAnythingApplication.java
@@ -2,10 +2,8 @@ package pl.sda.askanything;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class AskAnythingApplication {
 
     public static void main(String[] args) {
